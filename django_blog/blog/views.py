c9dc8a56bc6805cc325df50190f54b62cb6b7186
kalebm749 <kalebm749@gmail.com>
2026-06-28 11:28:26 -0600

    feat: add home and about html templates and render with views, update settings.py to include blog as installed app

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        "author": "MistaT",
+        "title": "Blog Post 1",
+        "content": "Jimmy crack corn",
+        "date_posted": "August 27th 2001",
+    },
+    {
+        "author": "GainT",
+        "title": "Blog Post 2",
+        "content": "Jimmy fucks corn",
+        "date_posted": "August 28th 2001",
+    },
+]
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, "blog/about.html")
